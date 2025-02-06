0838a77f7f4a194aac26f484753d960be8b3c10b
Liqianui <2369700429@qq.com>
2025-02-06 18:45:48 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+import App from './App'
+import uviewPlus from '@/uni_modules/uview-plus'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+
+ 
+ 
+ 
+// #ifndef VUE3
+import Vue from 'vue'
+import searchData from './pages/global/searchData.vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+Vue.component('search-data', searchData);
+App.mpType = 'app'
+ 
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+
+export function createApp() {
+	
+  const app = createSSRApp(App)
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+  app.use(uviewPlus)
+  app.use(ElementPlus)
+  return {
+    app
+  }
+}
+// #endif
